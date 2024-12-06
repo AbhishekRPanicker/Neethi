1b06016ebb628c69173625acc18daf4a26268a5b
Acube32 <99085216+Acube32@users.noreply.github.com>
2024-12-06 21:36:35 +0530

    Delete Neethi-main/Neethi-main directory

--- Neethi-main/Neethi-main/mysite/register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import register, log_in
-
-
-urlpatterns = [
-    path("register/", register, name="register"),
-    path('', include("django.contrib.auth.urls")),
-]
